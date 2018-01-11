1ed418223aa5c49c0f5fd1fe2b93787534006c9e
lmgracia <lmgracia@minsait.com>
2018-01-11 23:23:03 +0100

    SessionREgistry

--- controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/security/SpringSecurityConfig.java
+++ controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/security/SpringSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.indracompany.sofia2.controlpanel.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 import lombok.extern.slf4j.Slf4j;
