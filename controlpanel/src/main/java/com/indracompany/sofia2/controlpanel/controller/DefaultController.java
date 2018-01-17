397df5751833ae316d9472f598bff2b062cc5418
lmgracia <lmgracia@minsait.com>
2018-01-17 14:00:32 +0100

    license in sources

--- controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/controller/DefaultController.java
+++ controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/controller/DefaultController.java
@@ -1,17 +1,32 @@
+/**
+ * Copyright Indra Sistemas, S.A.
+ * 2013-2018 SPAIN
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.indracompany.sofia2.controlpanel.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
+//import net.minidev.json.JSONArray;
+//import net.minidev.json.JSONObject;
 
 @Controller
 public class DefaultController {
 
     @GetMapping("/")
     public String home1(Model model) {
+		return null;
+		/*
     	JSONObject navItem = new JSONObject();
     	JSONArray nav= new JSONArray();
     	navItem.put("title", "Ontologies");
@@ -34,6 +49,7 @@ public class DefaultController {
     	model.addAttribute("menu", nav);
     	System.out.println(nav);
     	return "/main";
+		*/
     }
 
     @GetMapping("/home")
