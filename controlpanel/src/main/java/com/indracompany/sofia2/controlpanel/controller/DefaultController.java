df74e2c4b8ba10905e2f5ba52bb4bac133dca659
lmgracia <lmgracia@minsait.com>
2018-01-17 15:07:48 +0100

    Updates

--- controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/controller/DefaultController.java
+++ controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/controller/DefaultController.java
@@ -17,39 +17,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//import net.minidev.json.JSONArray;
-//import net.minidev.json.JSONObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @Controller
 public class DefaultController {
 
     @GetMapping("/")
     public String home1(Model model) {
-		return null;
-		/*
-    	JSONObject navItem = new JSONObject();
-    	JSONArray nav= new JSONArray();
-    	navItem.put("title", "Ontologies");
-    	navItem.put("icon", "");
-    	navItem.put("url", "#");
-    	navItem.put("rol", "ROLE_OPERATIONS");
-    	JSONObject navItem11 = new JSONObject();
-    	navItem11.put("title", "Create Ontology");
-    	navItem11.put("icon", "");
-    	navItem11.put("url", "#");
-    	navItem11.put("rol", "ROLE_OPERATIONS");
-    	navItem.put("nav-item", navItem11);
-    	nav.add(navItem);
-    	JSONObject navItem2 = new JSONObject();
-    	navItem2.put("title", "Visualizations");
-    	navItem2.put("icon", "");
-    	navItem2.put("url", "#");
-    	navItem2.put("rol", "ROLE_OPERATIONS");
-    	nav.add(navItem2);
-    	model.addAttribute("menu", nav);
-    	System.out.println(nav);
-    	return "/main";
-		*/
+        return "/home";
     }
 
     @GetMapping("/home")
