8f64ad2b961b364fb0e3bcf82dbe311b2d7f5485
fjgcornejo <fjgcornejo@indra.es>
2018-01-18 13:49:15 +0100

    OntologyController

--- controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/controller/ontologies/OntologyController.java
+++ controlpanel/src/main/java/com/indracompany/sofia2/controlpanel/controller/ontologies/OntologyController.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright Indra Sistemas, S.A.
+ * 2013-2018 SPAIN
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.indracompany.sofia2.controlpanel.controller.ontologies;
 
 import java.util.List;
@@ -29,18 +42,29 @@ public class OntologyController {
 	public String listOntologies(Model uiModel,HttpServletRequest request)
 	{
 		
+		String identification = request.getParameter("identification");
+		String description = request.getParameter("description");
 		List<Ontology> ontologies;
 		Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
 		String userRole=authentication.getAuthorities().toArray()[0].toString();
 		if(userRole.equals("ROLE_ADMINISTRATOR"))
 		{
-			ontologies=this.ontologyRepository.findAll();
+			if(description!=null && identification!=null){
+				ontologies=this.ontologyRepository.findByIdentificationLikeAndDescriptionLike(identification, description);
+			}else{
+				ontologies=this.ontologyRepository.findAll();
+			}
 		}else
 		{
-			ontologies=this.ontologyRepository.findByUserId(authentication.getName());
+			if(description!=null && identification!=null){
+				ontologies=this.ontologyRepository.findByUserIdAndIdentificationLikeAndDescriptionLike(authentication.getName(), identification, description);
+			}else{
+				ontologies=this.ontologyRepository.findByUserId(authentication.getName());
+		
+			}
 		}
 		uiModel.addAttribute("ontologies",ontologies);
-		return "/ontologies/list";
+		return "ontologies/list";
 	}
 	
 	
