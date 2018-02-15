54709de6f5e926861fa4a80fc12c3431673d5e6f
fjgcornejo <fjgcornejo@indra.es>
2018-02-15 17:57:43 +0100

    Commit

---
+++ modules/api-manager/src/main/java/com/indracompany/sofia2/api/init/Product.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright Indra Sistemas, S.A.
+ * 2013-2018 SPAIN
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.indracompany.sofia2.api.init;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+
+/**
+ * Info osa_product
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "groupId",
+    "imageList",
+    "code",
+    "name",
+    "category",
+    "mainImage"
+})
+public class Product {
+
+    @JsonProperty("groupId")
+    private String groupId;
+    @JsonProperty("imageList")
+    private List<String> imageList = new ArrayList<String>();
+    @JsonProperty("code")
+    private String code;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("category")
+    private String category;
+    @JsonProperty("mainImage")
+    private String mainImage;
+
+    @JsonProperty("groupId")
+    public String getGroupId() {
+        return groupId;
+    }
+
+    @JsonProperty("groupId")
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    @JsonProperty("imageList")
+    public List<String> getImageList() {
+        return imageList;
+    }
+
+    @JsonProperty("imageList")
+    public void setImageList(List<String> imageList) {
+        this.imageList = imageList;
+    }
+
+    @JsonProperty("code")
+    public String getCode() {
+        return code;
+    }
+
+    @JsonProperty("code")
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("category")
+    public String getCategory() {
+        return category;
+    }
+
+    @JsonProperty("category")
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @JsonProperty("mainImage")
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    @JsonProperty("mainImage")
+    public void setMainImage(String mainImage) {
+        this.mainImage = mainImage;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(groupId).append(imageList).append(code).append(name).append(category).append(mainImage).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Product) == false) {
+            return false;
+        }
+        Product rhs = ((Product) other);
+        return new EqualsBuilder().append(groupId, rhs.groupId).append(imageList, rhs.imageList).append(code, rhs.code).append(name, rhs.name).append(category, rhs.category).append(mainImage, rhs.mainImage).isEquals();
+    }
+
+}
