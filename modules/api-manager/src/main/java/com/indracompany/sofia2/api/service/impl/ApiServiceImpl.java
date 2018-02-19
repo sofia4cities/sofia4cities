fa2814c43ce01eed93a468820dfe8209e7757a1b
ljsantos <ljsantos@indra.es>
2018-02-19 16:02:40 +0100

    more operations

--- modules/api-manager/src/main/java/com/indracompany/sofia2/api/service/impl/ApiServiceImpl.java
+++ modules/api-manager/src/main/java/com/indracompany/sofia2/api/service/impl/ApiServiceImpl.java
@@ -133,6 +133,9 @@ public class ApiServiceImpl extends ApiManagerService implements ApiServiceInter
 				if (QUERY_TYPE.equalsIgnoreCase("SQLLIKE")) {
 					OUTPUT = queryToolService.querySQLAsJson(ontology.getIdentification(), QUERY, 0);
 				}
+				else if (QUERY_TYPE.equalsIgnoreCase("NATIVE")) {
+					OUTPUT = queryToolService.queryNativeAsJson(ontology.getIdentification(), QUERY, 0,0);
+				}
 				else {
 					OUTPUT = mongoBasicOpsDBRepository.findById(ontology.getIdentification(), OBJECT_ID);
 				}
@@ -144,7 +147,7 @@ public class ApiServiceImpl extends ApiManagerService implements ApiServiceInter
 		}
 		else if (METHOD.equalsIgnoreCase(ApiOperation.Type.PUT.name())) {
 			
-			if (BODY!=null && BODY.length()>0) {
+			if (OBJECT_ID!=null && OBJECT_ID.length()>0) {
 				String updateQuery = "db."+ontology.getIdentification()+".update({\"_id\": {\"$oid\" : \""+OBJECT_ID+"\" }}, {$set:"+BODY+" })";
 				mongoBasicOpsDBRepository.updateNative(ontology.getIdentification(), updateQuery);	
 				OUTPUT = mongoBasicOpsDBRepository.findById(ontology.getIdentification(), OBJECT_ID);	
@@ -153,11 +156,18 @@ public class ApiServiceImpl extends ApiManagerService implements ApiServiceInter
 			else {
 				mongoBasicOpsDBRepository.updateNative(ontology.getIdentification(), BODY);	
 			}
-
-			
+	
 		}
 		else if (METHOD.equalsIgnoreCase(ApiOperation.Type.DELETE.name())) {
-			mongoBasicOpsDBRepository.deleteNative(ontology.getIdentification(), "{\"_id\": { \"$oid\" : \""+OBJECT_ID+"\" }}");
+			
+			if (OBJECT_ID!=null && OBJECT_ID.length()>0) {
+				mongoBasicOpsDBRepository.deleteNative(ontology.getIdentification(), "{\"_id\": { \"$oid\" : \""+OBJECT_ID+"\" }}");
+			}
+			
+			else {
+				mongoBasicOpsDBRepository.deleteNative(ontology.getIdentification(), BODY);	
+			}
+			
 		}
 				
 		data.put(ApiServiceInterface.OUTPUT, OUTPUT);
