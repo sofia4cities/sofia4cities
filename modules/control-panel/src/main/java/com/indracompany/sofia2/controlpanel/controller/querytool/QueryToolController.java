200bf879d3869512bbe710085e1a05efe9a3ff64
lmgracia <lmgracia@minsait.com>
2018-03-02 19:07:34 +0100

    refactoring persistence module

--- modules/control-panel/src/main/java/com/indracompany/sofia2/controlpanel/controller/querytool/QueryToolController.java
+++ modules/control-panel/src/main/java/com/indracompany/sofia2/controlpanel/controller/querytool/QueryToolController.java
@@ -75,12 +75,12 @@ public class QueryToolController {
 						ontologyIdentification);
 			if (hasUserPermission) {
 				if (queryType.toUpperCase().equals(QUERY_SQL)) {
-					queryResult = queryToolService.querySQLAsJson(ontologyIdentification, query, 0);
+					queryResult = queryToolService.querySQLAsJson(utils.getUserId(), ontologyIdentification, query, 0);
 					model.addAttribute("queryResult", queryResult);
 					return "querytool/show :: query";
 
 				} else if (queryType.toUpperCase().equals(QUERY_NATIVE)) {
-					queryResult = queryToolService.queryNativeAsJson(ontologyIdentification, query);
+					queryResult = queryToolService.queryNativeAsJson(utils.getUserId(), ontologyIdentification, query);
 					model.addAttribute("queryResult", utils.getAsObject(queryResult));
 					return "querytool/show :: query";
 				} else {
