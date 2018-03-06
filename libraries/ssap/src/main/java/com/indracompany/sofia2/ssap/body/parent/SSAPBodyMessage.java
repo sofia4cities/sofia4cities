739ebe93bdd838bb244fa9118ac07273a1e7fc8d
Juan Jose <jjmorenoa@minsait.com>
2018-03-06 05:17:34 +0100

    iotbroker: stomp over websocket gateway added v0

--- libraries/ssap/src/main/java/com/indracompany/sofia2/ssap/body/parent/SSAPBodyMessage.java
+++ libraries/ssap/src/main/java/com/indracompany/sofia2/ssap/body/parent/SSAPBodyMessage.java
@@ -13,6 +13,33 @@
  */
 package com.indracompany.sofia2.ssap.body.parent;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.indracompany.sofia2.ssap.body.SSAPBodyDeleteByIdMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyDeleteMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyEmptyMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyInsertMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyJoinMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyLeaveMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyQueryMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyReturnMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyUpdateByIdMessage;
+import com.indracompany.sofia2.ssap.body.SSAPBodyUpdateMessage;
+
+@JsonTypeInfo(use=Id.NAME)
+@JsonSubTypes({
+	@JsonSubTypes.Type(SSAPBodyDeleteByIdMessage.class),
+	@JsonSubTypes.Type(SSAPBodyDeleteMessage.class),
+	@JsonSubTypes.Type(SSAPBodyEmptyMessage.class),
+	@JsonSubTypes.Type(SSAPBodyInsertMessage.class),
+	@JsonSubTypes.Type(SSAPBodyJoinMessage.class),
+	@JsonSubTypes.Type(SSAPBodyLeaveMessage.class),
+	@JsonSubTypes.Type(SSAPBodyQueryMessage.class),
+	@JsonSubTypes.Type(SSAPBodyReturnMessage.class),
+	@JsonSubTypes.Type(SSAPBodyUpdateByIdMessage.class),
+	@JsonSubTypes.Type(SSAPBodyUpdateMessage.class)
+})
 public abstract class SSAPBodyMessage {
 
 	public abstract boolean isSessionKeyMandatory();
