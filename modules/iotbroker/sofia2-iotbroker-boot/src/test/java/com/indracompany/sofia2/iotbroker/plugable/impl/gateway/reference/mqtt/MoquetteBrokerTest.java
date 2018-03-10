220a753e90aabcfa9fae4757656145db8447ca08
Juan Jose <jjmorenoa@minsait.com>
2018-03-10 10:57:26 +0100

    iotbroker test adjustments after rebase

--- modules/iotbroker/sofia2-iotbroker-boot/src/test/java/com/indracompany/sofia2/iotbroker/plugable/impl/gateway/reference/mqtt/MoquetteBrokerTest.java
+++ modules/iotbroker/sofia2-iotbroker-boot/src/test/java/com/indracompany/sofia2/iotbroker/plugable/impl/gateway/reference/mqtt/MoquetteBrokerTest.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright Indra Sistemas, S.A.
+ * 2013-2018 SPAIN
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.indracompany.sofia2.iotbroker.plugable.impl.gateway.reference.mqtt;
 
 import static org.mockito.Matchers.any;
@@ -47,7 +60,7 @@ public class MoquetteBrokerTest {
 	String topic        = "/message";
 	String content      = "Message from MqttPublishSample";
 	int qos             = 2;
-	String broker_url       = "tcp://localhost:1883";
+	String broker_url       = "tcp://localhost:1888";
 	String clientId     = "JavaSample";
 	MemoryPersistence persistence = new MemoryPersistence();
 
