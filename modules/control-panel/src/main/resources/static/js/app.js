dbb8675aad882da5d28deb8561ef224637c73389
Alex Claramonte <aclaramonte@minsait.com>
2018-03-12 11:02:31 +0100

    ajustes datetimepicker

--- modules/control-panel/src/main/resources/static/js/app.js
+++ modules/control-panel/src/main/resources/static/js/app.js
@@ -607,22 +607,7 @@ var App = function() {
 					}
 				}
 				return this;
-			};
-			
-			
-			// DEFAULT ICONS FOR DATETIMEPICKERS ON FONT-AWESOME
-			$.fn.datetimepicker.defaults.icons = {
-				time: 'fa fa-clock-o',
-				date: 'fa fa-calendar',
-				up: 'fa fa-chevron-up',
-				down: 'fa fa-chevron-down',
-				previous: 'fa fa-chevron-left',
-				next: 'fa fa-chevron-right',
-				today: 'fa fa-dot-circle-o',
-				clear: 'fa fa-trash',
-				close: 'fa fa-times'
-			};
-			
+			};		
         },
 
         //main function to initiate core javascript after ajax complete
