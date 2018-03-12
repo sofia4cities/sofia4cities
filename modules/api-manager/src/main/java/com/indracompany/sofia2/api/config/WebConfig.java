631cfde56511b2a2e630b640322d2f03deee4e56
ljsantos <ljsantos@indra.es>
2018-03-12 14:06:34 +0100

    added custom html in swagger ui without topbar

---
+++ modules/api-manager/src/main/java/com/indracompany/sofia2/api/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.indracompany.sofia2.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+ 
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
+	
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    	if (!registry.hasMappingForPattern("/webjars/**"))
+    	{
+    		registry.addResourceHandler("/webjars/**").
+    			addResourceLocations("classpath:/META-INF/resources/webjars/")
+    	         .resourceChain(false);
+    		
+    		registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("/webjars/")
+            .resourceChain(false);
+    	}
+    	if (!registry.hasMappingForPattern("/**")) {
+    		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    	}
+    }
+}
