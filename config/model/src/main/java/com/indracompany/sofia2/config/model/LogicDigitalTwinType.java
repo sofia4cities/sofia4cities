0a6853159646d520e1aa87e9b67abde04191e77c
Raquel Barrio <rbarrio@minsait.com>
2018-03-12 15:13:48 +0100

    Add creation of digital twin types

---
+++ config/model/src/main/java/com/indracompany/sofia2/config/model/LogicDigitalTwinType.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright Indra Sistemas, S.A.
+ * 2013-2018 SPAIN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.indracompany.sofia2.config.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.hibernate.annotations.Type;
+import org.springframework.beans.factory.annotation.Configurable;
+
+import com.indracompany.sofia2.config.model.base.AuditableEntityWithUUID;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Configurable
+@Entity
+@Table(name = "LOGIC_DIGITAL_TWIN_TYPE")
+public class LogicDigitalTwinType extends AuditableEntityWithUUID{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@ManyToOne
+	@JoinColumn(name = "TYPE_ID", referencedColumnName = "ID", nullable = false)
+	@Getter
+	@Setter
+	private DigitalTwinType typeId;
+	
+	@Column(name = "logic", nullable = false)
+	@NotNull
+	@Lob
+	@Type(type = "org.hibernate.type.TextType")
+	@Getter
+	@Setter
+	private String logic;
+}
