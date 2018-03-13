ecc531070ba4cc0ea0553688f89f42eb4cd6b65d
Miguel Morán Cassy <danzig6661@gmail.com>
2018-03-13 08:20:44 +0100

    Bug fixes

--- docker-deployment/dockerfiles/nodered/sofia2-config-nodes-config.js
+++ docker-deployment/dockerfiles/nodered/sofia2-config-nodes-config.js
@@ -1,7 +1,7 @@
 var sofia2Config = {
-    "sibBasePath":"http://${SERVERNAME}",
-    "scriptBasePath":"http://${SERVERNAME}",
-	"consoleBasePath":"http://${SERVERNAME}"
+    "sibBasePath":"https://${SERVERNAME}",
+    "scriptBasePath":"https://${SERVERNAME}",
+	"consoleBasePath":"https://${SERVERNAME}"
 }
 
 module.exports = sofia2Config;
