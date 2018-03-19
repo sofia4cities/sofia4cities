16e2e25b32f09d374a2c0107901e0441268b10f9
cfsanchez <cfsanchez@indra.es>
2018-03-19 11:30:29 +0100

    Added a new rule to the api manager that checks that the data inserted is complaint with the ontology schema

--- config/services/src/test/java/com/indracompany/sofia2/config/services/ontologydata/OntologyDataServiceImplTest.java
+++ config/services/src/test/java/com/indracompany/sofia2/config/services/ontologydata/OntologyDataServiceImplTest.java
@@ -24,10 +24,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 @RunWith(MockitoJUnitRunner.class)
 public class OntologyDataServiceImplTest {
 	
+	
+
 	@InjectMocks
 	OntologyDataServiceImpl service;
 	
-	private String GOOD_JSON_SCHEMA = "{\n" + 
+	private final String GOOD_JSON_SCHEMA = "{\n" + 
 			"    \"$schema\": \"http://json-schema.org/draft-04/schema#\",\n" + 
 			"    \"title\": \"TMS-Commands\",\n" + 
 			"    \"type\": \"object\",\n" + 
@@ -56,10 +58,58 @@ public class OntologyDataServiceImplTest {
 			"    \"additionalProperties\": true\n" + 
 			"}";
 	
-	private String GOOD_JSON_DATA = "{\"Command\":{ \"id\":\"string\"}}";
+	private final String BAD_JSON_SCHEMA = "{\n" + 
+			"    \"$schema\": \"http://json-schema.org/draft-04/schema#\",\n" + 
+			"    \"title\": \"TMS-Commands\",\n" + 
+			"    \"type\": \"object\",\n" + 
+			"    \"required\": [\n" + 
+			"        \"Command\"\n" + 
+			"    ],\n" + 
+			"    \"properties\": {\n" + 
+			"        \"Command\": {\n" + 
+			"            \"type\": \"string\",\n" + 
+			"            \"$ref\": \"#/datos\"\n" + 
+			"        }\n" + 
+			"    },\n" + 
+			"    \"datos\": {\n" + 
+			"        \"description\": \"Info EmptyBase\",\n" + 
+			"        \"type\": \"object\",\n" + 
+			"        \"required\": [\n" + 
+			"            \"ERROR_ID_NAME\"\n" +   //Error in the id name
+			"        ],\n" + 
+			"        \"properties\": {\n" + 
+			"            \"id\": {\n" + 
+			"                \"type\": \"string\"\n" + 
+			"            }\n" + 
+			"        }\n" + 
+			"    },\n" + 
+			"    \"description\": \"Ontologia para comandos de TMS\",\n" + 
+			"    \"additionalProperties\": true\n" + 
+			"}";;
+	
+	private final String GOOD_JSON_DATA = "{\"Command\":{ \"id\":\"string\"}}";
+	
+	private final String NONVALID_JSON_DATA = "{\"Something\":{ \"id\":\"string\"}}"; //Something is not declared in the schema
+	
+	private final String BAD_JSON_DATA = "{Something\":{ \"id\":\"string\"}}"; //invalid JSON. Something should be surrounded by quotes.
 	
 	@Test
 	public void given_OneValidJsonSchemaAndOneCompliantJson_When_TheJsonIsValidated_Then_ItReturnsTrue() {
-		assertTrue("This Json data should pass the validation",service.isJsonCompliantWithSchema(GOOD_JSON_DATA, GOOD_JSON_SCHEMA));
+		assertTrue("This Json data should pass the validation", service.isJsonCompliantWithSchema(GOOD_JSON_DATA, GOOD_JSON_SCHEMA));
+	}
+	
+	@Test
+	public void given_OneInvalidJsonSchemaAndOneCompliantJson_When_TheJsonIsValidated_Then_ItResturnsFalse() {
+		assertFalse("The schema should not be validated", service.isJsonCompliantWithSchema(GOOD_JSON_DATA, BAD_JSON_SCHEMA));
+	}
+	
+	@Test
+	public void given_OneValidJsonSchemaAndOneNotCompliantJson_When_TheJsonIsValidated_Then_ItReturnsFalse() {
+		assertFalse("The Json data should not pass the validation", service.isJsonCompliantWithSchema(NONVALID_JSON_DATA, GOOD_JSON_SCHEMA));
+	}
+	
+	@Test
+	public void given_OneValidJsonSchemaAndOneIncorrectJson_When_TheJsonIsValidated_Then_ItReturnsFalse() {
+		assertFalse("The Json data is incorrect, it should not be validated", service.isJsonCompliantWithSchema(BAD_JSON_DATA, GOOD_JSON_SCHEMA));
 	}
 }
