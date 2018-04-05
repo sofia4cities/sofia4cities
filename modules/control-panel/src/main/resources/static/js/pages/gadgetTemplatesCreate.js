22d390290dfa8d9ad2a471f3a6bbc853289c7d63
dsanteodoro <dsanteodoro@indra.es>
2018-04-05 15:44:50 +0200

    templates and parameters first iteration

--- modules/control-panel/src/main/resources/static/js/pages/gadgetTemplatesCreate.js
+++ modules/control-panel/src/main/resources/static/js/pages/gadgetTemplatesCreate.js
@@ -7,7 +7,7 @@ var GadgetsTemplateCreateController = function() {
 	var LANGUAGE = ['es'];
 	var currentLanguage = ''; // loaded from template.	
 	var internalLanguage = 'en';	
-	
+	var myCodeMirror;
 	// CONTROLLER PRIVATE FUNCTIONS	
 	
 	var navigateUrl = function(url){ window.location.href = url; }
@@ -29,7 +29,7 @@ var GadgetsTemplateCreateController = function() {
 		logControl ? console.log('handleCodeMirror() on -> templateCode') : '';	
 		
         var myTextArea = document.getElementById('templateCode');
-        var myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
+            myCodeMirror = CodeMirror.fromTextArea(myTextArea, {
         	mode: "code",
             lineNumbers: true,
             foldGutter: true,
@@ -40,31 +40,231 @@ var GadgetsTemplateCreateController = function() {
         })
 		myCodeMirror.setSize("100%", 350);
         myCodeMirror.on('change',editor => { var scope = angular.element(document.getElementsByTagName('livehtml')[0]).scope();
-		  scope.$$childHead.vm.livecontent=editor.getValue();
-          scope.$$childHead.vm.$onChanges([]);          
-        });    
+		  scope.$$childHead.vm.livecontent=editor.getValue();		  
+          scope.$$childHead.vm.$onChanges([]);  
+          searchProperties(editor.getValue());
+         
+        })    
+        
+        myCodeMirror.on("drop",function(editor,e,searchProperties) { 
+        	var data = e.dataTransfer.getData("content");   
+        	e.preventDefault();
+        	myCodeMirror.replaceRange(dataFromId(data), CodeMirror.Pos(myCodeMirror.lastLine()));       
+        });
+        searchProperties(myCodeMirror.getValue());
+        
     }
+	
+	
+	// FORM VALIDATION
+	var handleValidation = function() {
+		logControl ? console.log('handleValidation() -> ') : '';
+		// for more info visit the official plugin documentation:
+		// http://docs.jquery.com/Plugins/Validation
+
+		var form1 = $('#gadget_create_form');
+		var error1 = $('.alert-danger');
+		var success1 = $('.alert-success');
+
+		// set current language
+		currentLanguage = templateCreateReg.language || LANGUAGE;
+
+		form1.validate({
+					errorElement : 'span', // default input error message
+											// container
+					errorClass : 'help-block help-block-error', // default input
+																// error message
+																// class
+					focusInvalid : false, // do not focus the last invalid
+											// input
+					ignore : ":hidden:not(.selectpicker)", // validate all
+															// fields including
+															// form hidden input
+															// but not
+															// selectpicker
+					lang : currentLanguage,
+					// custom messages
+					messages : {
+
+					},
+					// validation rules
+					rules : {
+						identification : {
+							minlength : 5,
+							required : true
+						},
+						description : {
+							minlength : 5,
+							required : true
+						},
+
+					},
+					invalidHandler : function(event, validator) { // display
+																	// error
+																	// alert on
+																	// form
+																	// submit
+						success1.hide();
+						error1.show();
+						App.scrollTo(error1, -200);
+					},
+					errorPlacement : function(error, element) {
+						if (element.is(':checkbox')) {
+							error
+									.insertAfter(element
+											.closest(".md-checkbox-list, .md-checkbox-inline, .checkbox-list, .checkbox-inline"));
+						} else if (element.is(':radio')) {
+							error
+									.insertAfter(element
+											.closest(".md-radio-list, .md-radio-inline, .radio-list,.radio-inline"));
+						} else {
+							error.insertAfter(element);
+						}
+					},
+					highlight : function(element) { // hightlight error inputs
+						$(element).closest('.form-group').addClass('has-error');
+					},
+					unhighlight : function(element) { // revert the change
+														// done by hightlight
+						$(element).closest('.form-group').removeClass(
+								'has-error');
+					},
+					success : function(label) {
+						label.closest('.form-group').removeClass('has-error');
+					},
+					// ALL OK, THEN SUBMIT.
+					submitHandler : function(form) {
+
+						 success1.show();
+			                error1.hide();
+							form.submit();
+					}
+				});
+	}
+	
+	
+	
+	function dataFromId(id){
+		var ident = new Uint32Array(1);
+		window.crypto.getRandomValues(ident);
+		
+		switch(id) {
+	    case "label_text":
+	        return '\n<label-s4c  name="parameterName-'+ident+'" type="text">label-text-s4c</label-s4c>';
+	        break;
+	    case "label_number":
+	    	return '\n<label-s4c name="parameterName-'+ident+'" type="number">label-number-s4c</label-s4c>';
+	        break;
+	    case "label_ds":
+	    	return '\n<label-s4c name="parameterName-'+ident+'" type="ds" >label-ds-s4c</label-s4c>';
+	        break;	  
+	    case "select_options":
+	    	return `\n<select-s4c name="parameterName-`+ident+`" type="ds" >label-select-s4c\n\t <option-s4c value=""></option-s4c>\n</select-s4c>`;
+	    			
+	    	        
+	        break;
+	    default:
+	        return "";
+	}
+		
+	}
+	
+
+	function searchTag(regex,str,end){
+		let m;
+		let found=[];
+		while ((m = regex.exec(str)) !== null) {  
+		    if (m.index === regex.lastIndex) {
+		        regex.lastIndex++;
+		    }
+		    m.forEach(function(item, index, arr){
+			if(end){
+				found.push(arr.index+arr[0].length);
+			}else{
+				found.push(arr.index);
+			}
+			});  
+		}
+		return found;
+	}
+
+
+	
+	function searchProperties(str){
+
+		const regexLabelStart = /<\s*label-s4c/g;
+		const regexLabelEnd = /<\/\s*label-s4c\s*>/g;
+
+		const regexSelectStart = /<\s*select-s4c/g;
+		const regexSelectEnd = /<\/\s*select-s4c\s*>/g;
+
+	
+		let foundLabelStart=[];
+		let foundLabelEnd=[];
+		let foundSelectStart=[];
+		let foundSelectEnd=[];
+
+
+		foundLabelStart = searchTag(regexLabelStart,str,false);
+		foundLabelEnd = searchTag(regexLabelEnd,str,true);
+		foundSelectStart = searchTag(regexSelectStart,str,false);
+		foundSelectEnd = searchTag(regexSelectEnd,str,true);
+
+		$('#parameters-form').empty();
+		$('#parameters-form').append('<li class="list-group-item active">Estos son los componentes paramétricos de la plantilla</li>');
+
+		for (var i = 0; i < foundLabelStart.length; i++) {			
+			var tag = str.substring(foundLabelStart[i],foundLabelEnd[i]);
+			if(tag.replace(/\s/g, '').search('type="text"')>=0){
+				var elem = $(tag);
+				$('#parameters-form').append('<li class="list-group-item"><label class="bold">'+elem[0].getAttribute(["name"])+'&nbsp:&nbsp</label><label>'+gadgetTemplateCreateJson.parameterTextLabel+'</label></li>');
+			}else if(tag.replace(/\s/g, '').search('type="number"')>=0){
+				var elem = $(tag);
+				$('#parameters-form').append('<li class="list-group-item"><label class="bold">'+elem[0].getAttribute(["name"])+'&nbsp:&nbsp</label><label>'+gadgetTemplateCreateJson.parameterNumberLabel+'</label></li>');
+			}else if(tag.replace(/\s/g, '').search('type="ds"')>=0){
+				var elem = $(tag);
+				$('#parameters-form').append('<li class="list-group-item"><label class="bold">'+elem[0].getAttribute(["name"])+'&nbsp:&nbsp</label><label>'+gadgetTemplateCreateJson.parameterDsLabel+'</label></li>');
+			}
+			
+		} 
+		 
+		for (var i = 0; i < foundSelectStart.length; i++) {
+			var tag = str.substring(foundLabelStart[i],foundLabelEnd[i]);
+			var elem = $(tag);
+			$('#parameters-form').append('<li class="list-group-item"><label class="bold">'+elem[0].getAttribute(["name"])+'&nbsp:&nbsp</label><label>'+gadgetTemplateCreateJson.parameterSelectLabel+'</label></li>');
+		} 
+		 
+		}
+
+
+
+	
 	
 	var updatePreview = function (){
 		var scope = angular.element(document.getElementsByTagName('livehtml')[0]).scope();
 		scope.$$childHead.vm.livecontent=$('#templateCode').val();
+		
         scope.$$childHead.vm.$onChanges([]);  
 		
 	}
+	
+	 var drag = function (ev) {		 
+		    ev.dataTransfer.setData("content", ev.target.id);
+		}	
 	
 	// CONTROLLER PUBLIC FUNCTIONS 
 	return{		
 		// LOAD() JSON LOAD FROM TEMPLATE TO CONTROLLER
 		load: function(Data) { 
 			logControl ? console.log(LIB_TITLE + ': load()') : '';
-			return ontologyCreateReg = Data;
+			return templateCreateReg = Data;
 		},	
 		
 		// INIT() CONTROLLER INIT CALLS
 		init: function(){
 			logControl ? console.log(LIB_TITLE + ': init()') : '';
 			handleCodeMirror();
-		//	updatePreview();
+			handleValidation();
 		},
 		
 		// REDIRECT
@@ -75,10 +275,14 @@ var GadgetsTemplateCreateController = function() {
 		updatePreview: function(){
 			updatePreview();
 		},
+	
 		// DELETE GADGET DATASOURCE 
 		deleteGadgetTemplate: function(gadgetId){
 			logControl ? console.log(LIB_TITLE + ': deleteGadget()') : '';	
 			deleteGadgetTemplateConfirmation(gadgetId);			
+		},
+		drag: function(ev){
+			drag(ev);
 		}
 		
 	};
@@ -88,7 +292,7 @@ var GadgetsTemplateCreateController = function() {
 jQuery(document).ready(function() {
 	
 	// LOADING JSON DATA FROM THE TEMPLATE (CONST, i18, ...)
-	GadgetsTemplateCreateController.load();	
+	GadgetsTemplateCreateController.load(gadgetTemplateCreateJson);	
 		
 	// AUTO INIT CONTROLLER.
 	GadgetsTemplateCreateController.init();
