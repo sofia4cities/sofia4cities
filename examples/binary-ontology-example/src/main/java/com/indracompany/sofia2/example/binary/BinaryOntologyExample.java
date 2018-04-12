83fbe0faffd7e88ff7f60ee734158d1f38c30880
fjgcornejo <fjgcornejo@indra.es>
2018-04-12 15:29:03 +0200

    Binary ontology example.

--- examples/binary-ontology-example/src/main/java/com/indracompany/sofia2/example/binary/BinaryOntologyExample.java
+++ examples/binary-ontology-example/src/main/java/com/indracompany/sofia2/example/binary/BinaryOntologyExample.java
@@ -24,9 +24,11 @@ public class BinaryOntologyExample {
 
 		client.connect(token, clientPlatform, clientPlatformInstance);
 
-		BinarySerializer serializer = new BinarySerializer();
 		String pathFile = "S:\\sofia2-s4c\\examples\\binary-ontology-example\\logo_S4C.png";
 		String outputPath = "S:\\sofia2-s4c\\examples\\binary-ontology-example\\";
+
+		BinarySerializer serializer = new BinarySerializer();
+
 		JsonNode image = mapper.createObjectNode();
 		try {
 			image = serializer.getJsonBinary("File", new File(pathFile), Mime.IMAGE_PNG);
@@ -34,14 +36,16 @@ public class BinaryOntologyExample {
 			System.out.println("File not found");
 			e.printStackTrace();
 		}
-		String ontIns = "{ \"Ticket\" : { \"Identification\" : \"Road\", \"Status\" : \"STOPPED\", \"Email\" : \"corne93fktm@gmail.com\", \"Name\" : \"Javier\", \"Response_via\" : \"Email\", \"Coordinates\" : { \"coordinates\" : { \"0\" : 40.5295428, \"1\" : -3.641471 }, \"type\" : \"Point\" }, \"Type\" : \"ROAD\", \"Description\" : \"Roads in bad shape\" }, \"contextData\" : { \"clientPatform\" : \"Ticketing App\", \"clientPatformInstance\" : \"Ticketing App: Web\", \"clientConnection\" : \"\", \"clientSession\" : \"5c4b87d2-9d43-4cb7-851e-b14b4d023f2b\", \"user\" : \"developer\", \"timezoneId\" : \"Europe/Paris\", \"timestamp\" : \"Wed Apr 11 17:11:36 CEST 2018\" } }";
+
+		String ontIns = "{ \"Ticket\" : { \"Identification\" : \"Road\", \"Status\" : \"STOPPED\", \"Email\" : \"some@gmail.com\", \"Name\" : \"Javier\", \"Response_via\" : \"Email\", \"Coordinates\" : { \"coordinates\" : { \"0\" : 40.5295428, \"1\" : -3.641471 }, \"type\" : \"Point\" }, \"Type\" : \"ROAD\", \"Description\" : \"Roads in bad shape\" }, \"contextData\" : { \"clientPatform\" : \"Ticketing App\", \"clientPatformInstance\" : \"Ticketing App: Web\", \"clientConnection\" : \"\", \"clientSession\" : \"5c4b87d2-9d43-4cb7-851e-b14b4d023f2b\", \"user\" : \"developer\", \"timezoneId\" : \"Europe/Paris\", \"timestamp\" : \"Wed Apr 11 17:11:36 CEST 2018\" } }";
 		JsonNode object = mapper.readTree(ontIns);
 		((ObjectNode) object.path(ontology)).set("File", image.path("File"));
 
+		// perform POST
 		client.insertInstance(ontology, object.toString());
+
+		// perform GET
 		List<JsonNode> instances = client.getOntologyInstances(ontology);
-		if (instances.size() == 0)
-			client.insertInstance(ontology, object.toString());
 
 		for (JsonNode ontologyInstance : instances) {
 			if (!ontologyInstance.path(ontology).path("File").isMissingNode()) {
