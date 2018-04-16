1dcca2debb3445882722a1ec33655c8672cd843b
ljsantos <ljsantos@indra.es>
2018-04-16 15:43:09 +0200

    change package for tests

--- persistence/elasticsearch-impl/src/test/java/com/indracompany/sofia2/persistence/elasticsearch/ElasticSearchBySQLPluginIntegrationTest.java
+++ persistence/elasticsearch-impl/src/test/java/com/indracompany/sofia2/persistence/elasticsearch/ElasticSearchBySQLPluginIntegrationTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.indracompany.sofia2.persistence.mongodb;
+package com.indracompany.sofia2.persistence.elasticsearch;
 
 import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
 import org.elasticsearch.action.bulk.BulkResponse;
