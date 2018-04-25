1d10d6df93b53e1fc2196a2d9bb846a5c89d1596
pgmarquina <pgmarquina@indra.es>
2018-04-25 11:09:22 +0200

    DataModels menu

--- modules/control-panel/src/main/java/com/indracompany/sofia2/controlpanel/controller/datamodel/DataModelController.java
+++ modules/control-panel/src/main/java/com/indracompany/sofia2/controlpanel/controller/datamodel/DataModelController.java
@@ -13,18 +13,33 @@
  */
 package com.indracompany.sofia2.controlpanel.controller.datamodel;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.indracompany.sofia2.config.model.Configuration;
 import com.indracompany.sofia2.config.model.DataModel;
+import com.indracompany.sofia2.config.model.User;
+import com.indracompany.sofia2.config.model.WebProject;
 import com.indracompany.sofia2.config.services.datamodel.DataModelService;
+import com.indracompany.sofia2.config.services.exceptions.DataModelServiceException;
+import com.indracompany.sofia2.config.services.exceptions.UserServiceException;
+import com.indracompany.sofia2.config.services.exceptions.WebProjectServiceException;
+import com.indracompany.sofia2.config.services.user.UserService;
 import com.indracompany.sofia2.controlpanel.utils.AppWebUtils;
 
 import lombok.extern.slf4j.Slf4j;
@@ -36,6 +51,9 @@ public class DataModelController {
 
 	@Autowired //TODO use final and use constructor injection.
 	private DataModelService dataModelService;
+	
+	@Autowired
+	private UserService userService;
 	
 	@Autowired
 	private AppWebUtils utils;
@@ -73,27 +91,120 @@ public class DataModelController {
 		}
 
 		return "datamodels/list";
+	}	
+	
+	private void populateFormData(Model model) {
+		model.addAttribute("dataModelTypes", this.dataModelService.getAllDataModelsTypes());
+	}
+
+	@GetMapping(value = "/show/{id}", produces = "text/html")
+	public String show(Model model, @PathVariable("id") String id, RedirectAttributes redirect) {
+		DataModel dataModel= null;	
+		if (id != null) {
+			dataModel = this.dataModelService.getDataModelById(id);
+		}
+		if (dataModel == null)
+			return "error/404";
+		if (!this.utils.getUserId().equals(	dataModel.getUser().getUserId()) && !utils.isAdministrator())
+			return "error/403";
+		model.addAttribute("dataModel", dataModel);
+		return "datamodels/show";
+	}
+
+	@PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
+	@GetMapping(value = "/create", produces = "text/html")
+	public String create(Model model, @Valid DataModel dataModel, BindingResult bindingResult) {
+
+		this.populateFormData(model);
+		if (bindingResult.hasErrors())
+			model.addAttribute("datamodel", new DataModel()); 
+		return "/datamodels/create";
 	}
 	
-	@GetMapping("/show/{id}")
-	public String show(Model model, @PathVariable("id") String id, RedirectAttributes redirect) {
-		try {
-			DataModel dataModel = dataModelService.getDataModelById(id);
-			if (dataModel != null) {
+	@PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
+	@PostMapping(value = "/create", produces = "text/html")
+	public String createDataModel(Model model, @ModelAttribute DataModel datamodel, BindingResult bindingResult, RedirectAttributes redirect, HttpServletRequest request) {
 
-				model.addAttribute("dataModel", dataModel);
-				return "datamodels/show";
-				
-			} else {
-				utils.addRedirectMessage("datamodel.notfound.error", redirect);
-				return "redirect:/datamodels/list";
-			}
-		} catch(RuntimeException e) {
+		if(dataModelService.dataModelExists(datamodel)) {
+			log.error("This DataModel already exist" );
+			utils.addRedirectMessage("datamodel.error.exist", redirect);
 			return "redirect:/datamodels/list";
 		}
+		try {
+			datamodel.setUser(userService.getUserByIdentification(utils.getUserId()));
+			dataModelService.createDataModel(datamodel);
+			log.error("DataModel created correctly");
+			return "redirect:/datamodels/list"; // show/"+ datamodel.getId(); //show/"+ datamodel.getId();
 			
+		}catch (DataModelServiceException e) {
+			log.error("Cannot create datamodel " );
+			utils.addRedirectMessage("datamodel.error.created", redirect);
+			return "redirect:/datamodels/create";
+		}
 	}
-	
+	 
+	@PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
+	@GetMapping(value = "/update/{id}")
+	public String update(@PathVariable String id, Model model, HttpServletRequest request) {
+		
+		try {
+			this.populateFormData(model);
+			DataModel datamodel = this.dataModelService.getDataModelById(id);
+			if(datamodel != null) {
+				log.debug("Update Data Model");
+				model.addAttribute("datamodel", datamodel);
+				return "datamodels/create";
+			}else
+				return "datamodels/create";
+		}catch (DataModelServiceException e) {
+			return "datamodels/create";
+		}	
+	}
 
 	
+	@PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
+	@PutMapping(value="/update/{id}" , produces = "text/html")
+	public String updateDataModel(@PathVariable String id, Model model, @ModelAttribute DataModel datamodel, RedirectAttributes redirect, HttpServletRequest request) {
+				
+		if(datamodel != null) {
+			datamodel.setUser(userService.getUserByIdentification(utils.getUserId()));
+			if(!this.utils.getUserId().equals(datamodel.getUser().getUserId()) && !utils.isAdministrator())
+				return "error/403";
+			try {
+				this.dataModelService.updateDataModel(datamodel);
+			}catch(DataModelServiceException e) {
+				log.debug("Could not update the Data Model");
+				utils.addRedirectMessage("datamodel.error.update", redirect);
+				return "datamodels/create";
+			}
+		}else {
+			return "redirect:/update/" + datamodel.getId();
+		}
+			model.addAttribute("Datamodel", datamodel);
+			log.debug("Data Mode has been update succesfully");
+			return "redirect:/datamodels/show/" + datamodel.getId();
+	}
+	
+	@PreAuthorize("hasRole('ROLE_ADMINISTRATOR')")
+	@DeleteMapping("/delete/{id}")
+	public String deleteDataModel(Model model, @PathVariable("id") String id, RedirectAttributes redirect) {
+		
+		DataModel dataModel = dataModelService.getDataModelById(id);
+		if (dataModel != null) {
+			try {
+				this.dataModelService.deleteDataModel(id);
+			}catch (DataModelServiceException e) {
+				log.debug("Could not delete the Data Model");
+				utils.addRedirectMessage("datamodel.error.delete", redirect);
+				return "redirect:/datamodels/list";
+			}
+			log.debug("The Data Model has been created correctly");
+			return "redirect:/datamodels/list";
+		}else {
+			log.debug("The Data Model does not exist");
+			return "redirect:/datamodels/list";
+		}
+	}
+	
 }
+
