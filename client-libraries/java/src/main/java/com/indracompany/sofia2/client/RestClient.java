1078bcb948b3710b28152fee2a4957ef69395982
cfsanchez <cfsanchez@indra.es>
2018-05-11 09:57:04 +0200

    Add change in the chatbot

--- client-libraries/java/src/main/java/com/indracompany/sofia2/client/RestClient.java
+++ client-libraries/java/src/main/java/com/indracompany/sofia2/client/RestClient.java
@@ -1,14 +1,25 @@
+/**
+ * Copyright Indra Sistemas, S.A.
+ * 2013-2018 SPAIN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.indracompany.sofia2.client;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.indracompany.sofia2.ssap.enums.SSAPQueryType;
 
@@ -64,6 +75,7 @@ public class RestClient {
 		Request request = new Request.Builder().url(urlJoinWithParams).get().build();
 
 		client = new OkHttpClient();
+
 		Response response = client.newCall(request).execute();
 		log.info("Trying to join Iotbroker...");
 		JsonNode session = mapper.readTree(response.body().string());
