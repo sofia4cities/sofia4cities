326febfec315df55ee96216ec8df2bf2a7e12c80
ljsantos <ljsantos@indra.es>
2018-05-17 17:22:13 +0200

    introducing kafka login module adapted to ControlPanel open-api check device. Need to test with a real kafka implementation
    Former-commit-id: 46684a6be09ba1102ecdb53f06fa40446a227f91

--- config/init/src/main/java/com/indracompany/sofia2/systemconfig/init/InitConfigDB.java
+++ config/init/src/main/java/com/indracompany/sofia2/systemconfig/init/InitConfigDB.java
@@ -19,6 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
